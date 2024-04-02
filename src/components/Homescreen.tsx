d7e63561ca9aa4ec9fec7f72959f25369cd63e4e
Flavio Murata <flaviomurata02@gmail.com>
2024-04-02 10:31:57 -0300

    feat(homepage): implement homescreen animation

--- src/components/Homescreen.tsx
+++ src/components/Homescreen.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import { useRef } from 'react'
+import Lottie, { LottieRefCurrentProps } from 'lottie-react'
+
+import animationData from '../assets/pigAnimation.json'
 
 function Homescreen() {
-  const onSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-  }
+  const pigAnimation = useRef<LottieRefCurrentProps>(null)
   return (
     <>
       <h1>Bem Vindo ao MUVI Bank</h1>
+      <Lottie lottieRef={pigAnimation} animationData={animationData} />
       <br />
-      <h3>Insira o URL da sua API</h3>
-      <form onSubmit={onSubmit}>
+      <form>
         <div className="form-control">
-          <input type="text" />
+          <input type="text" placeholder="Insira o URL da sua API aqui" />
         </div>
         <button type="submit">Submit</button>
       </form>
