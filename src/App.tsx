a63ac2a3c59aabeda6076b1d47ad51e011c9d883
Vinicius Berti <vinicius.berti33@gmail.com>
2024-04-18 14:20:29 -0300

    feat: deposit-screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Accountscreen from './components/AccountScreen/AccountScreen'
 import Homescreen from './components/HomeScreen/Homescreen'
+import Depositscreen from './components/DepositScreen/DepositScreen'
 import './App.css'
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/home" element={<Homescreen />} />
-        <Route path="/" element={<Accountscreen />} />
+        <Route path="/account" element={<Accountscreen />} />
+        <Route path="/deposit" element={<Depositscreen />} />
       </Routes>
     </Router>
   )
