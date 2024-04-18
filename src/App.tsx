0590e5fedfeac5d0b3f4ab49100b3066e3b2ee9d
Vinicius Berti <vinicius.berti33@gmail.com>
2024-04-18 14:35:58 -0300

    feat: withdrawal-screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Accountscreen from './components/AccountScreen/AccountScreen'
 import Homescreen from './components/HomeScreen/Homescreen'
 import './App.css'
+import Withdrawal from './components/WithdrawalScreen/WithdrawalScreen'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/home" element={<Homescreen />} />
-        <Route path="/" element={<Accountscreen />} />
+        <Route path="/account" element={<Accountscreen />} />
+        <Route path="/withdrawal" element={<Withdrawal />} />
       </Routes>
     </Router>
   )
