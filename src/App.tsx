31a69ebf810a11eb79e7757545a59b8f40421043
Flavio Murata <flaviomurata02@gmail.com>
2024-04-18 17:44:13 -0300

    bugfix: fix css modules conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import Accountscreen from './components/AccountScreen/AccountScreen'
-import Homescreen from './components/HomeScreen/Homescreen'
-import Depositscreen from './components/DepositScreen/DepositScreen'
 import './App.css'
+import Accountscreen from './components/AccountScreen/AccountScreen'
+import Depositscreen from './components/DepositScreen/DepositScreen'
+import Homescreen from './components/HomeScreen/Homescreen'
 import Withdrawal from './components/WithdrawalScreen/WithdrawalScreen'
 
 function App() {
@@ -10,8 +10,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/home" element={<Homescreen />} />
-        <Route path="/account" element={<Accountscreen />} />
-        <Route path="/withdrawal" element={<Withdrawal />} />
+        <Route path="/" element={<Accountscreen />} />
+        <Route path="/withdraw" element={<Withdrawal />} />
         <Route path="/deposit" element={<Depositscreen />} />
       </Routes>
     </Router>
