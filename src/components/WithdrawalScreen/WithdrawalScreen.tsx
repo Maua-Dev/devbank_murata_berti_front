f85f8aebf2264d2803d7020789c6ce73e2dba323
Vinicius Berti <vinicius.berti33@gmail.com>
2024-04-21 14:22:04 -0300

    fix: fixing css

--- src/components/WithdrawalScreen/WithdrawalScreen.tsx
+++ src/components/WithdrawalScreen/WithdrawalScreen.tsx
@@ -1,44 +1,75 @@
+import { useNavigate } from 'react-router-dom'
 import classes from './WithdrawalScreen.module.css'
 
 export default function Withdrawal() {
+  const navigate = useNavigate()
+  
   return (
     <main className={classes.container}>
       <section>
-        <h1>Tela de Saque</h1>
-        <div className={classes.square}>
+        <h1 className={classes.title}>Tela de Saque</h1>
+        <div className={`${classes.square} ${classes.marginBottom40}`}>
           <div className={classes.info}>
-            <p>R$0,00</p>
+            <p className={classes.amount}>R$0,00</p>
           </div>
         </div>
         <div className={classes.buttoncontainer1}>
-          <button className={classes.button2} type="submit">
-            Depositar
+          <button
+            className={`${classes.button} ${classes.button2}`}
+            type="submit"
+            onClick={() => navigate('/')}
+          >
+            Sacar
           </button>
-          <button className={classes.button3} type="submit">
+          <button
+            className={`${classes.button} ${classes.button3}`}
+            type="submit"
+          >
             Limpar
           </button>
         </div>
       </section>
       <section className={classes.buttoncontainer2}>
-        <button className={classes.button1p} type="submit">
+        <button
+          className={`${classes.button} ${classes.button1p}`}
+          type="submit"
+        >
           R$2,00
         </button>
-        <button className={classes.button1p} type="submit">
+        <button
+          className={`${classes.button} ${classes.button1p}`}
+          type="submit"
+        >
           R$5,00
         </button>
-        <button className={classes.button1p} type="submit">
+        <button
+          className={`${classes.button} ${classes.button1p}`}
+          type="submit"
+        >
           R$10,00
         </button>
-        <button className={classes.button1q} type="submit">
+        <button
+          className={`${classes.button} ${classes.button1q}`}
+          type="submit"
+        >
           R$20,00
         </button>
-        <button className={classes.button1q} type="submit">
+        <button
+          className={`${classes.button} ${classes.button1q}`}
+          type="submit"
+        >
           R$50,00
         </button>
-        <button className={classes.button1q} type="submit">
+        <button
+          className={`${classes.button} ${classes.button1q}`}
+          type="submit"
+        >
           R$100,00
         </button>
-        <button className={classes.button1t} type="submit">
+        <button
+          className={`${classes.button} ${classes.button1t}`}
+          type="submit"
+        >
           R$200,00
         </button>
       </section>
