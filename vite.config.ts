0ae6816427619fa1e3df11e002b04763d038b864
EnricoSantarelli <enricosantarelli@hotmail.com>
2024-04-28 22:15:44 -0300

    test exclude iac on build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    manifest: true,
+    rollupOptions: {
+      external: [
+        "./iac"
+      ],
+    },
+  },
   css: {
     modules: {
       localsConvention: 'camelCase'
